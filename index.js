6663d0a0e730eb4ee70b195bbbcd4745ba62925d
pragmatic-form-3566 <chelsiasulin@gmail.com>
2018-12-30 22:05:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,10 +2,11 @@
 function theBeatlesPlay (musiciansArr, instrumentsArr) {
   var emptyArr = []
   for (let i=0; i<musiciansArr.length; i++) {
-    // let musician = musiciansArr[i]
-    // for (let j=0; j<instrumentsArr.length; j++)
-    // let instrument = instrumentsArr[j]
     emptyArr.push(`${musiciansArr[i]} plays ${instrumentsArr[i]}`)
   }
   return emptyArr
+}
+
+function johnLennonFacts (factsArr) {
+
 }
